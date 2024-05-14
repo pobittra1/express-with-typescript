f8e444042274af0400fa3ed83f844c4552de96a2
pobi <pobittrachandraroy2001@gmail.com>
2024-05-14 19:10:15 +0600

    implement routing object in express js

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,6 +4,24 @@ const app = express();
 //parsers
 app.use(express.json());
 
+//create Router object
+const userRouter = express.Router();
+
+//use router
+app.use("/api/v1/users", userRouter);
+
+//create userRouter listener
+userRouter.get("/create-user", (req: Request, res: Response) => {
+  const user = req.body;
+  console.log(user);
+  res.json({
+    succuess: true,
+    message: "user created successfully",
+    data: user,
+  });
+});
+
+//for params implementation
 app.get("/:userId/:subId", (req: Request, res: Response) => {
   console.log(req.params);
   console.log(req.params.userId);
@@ -11,10 +29,15 @@ app.get("/:userId/:subId", (req: Request, res: Response) => {
   res.send("Hello simran");
 });
 
+//for query implementation
+app.get("/", (req: Request, res: Response) => {
+  console.log(req.query);
+});
+
 app.post("/", (req: Request, res: Response) => {
   console.log(req.body);
   res.json({
-    name:"somothing"
+    name: "somothing",
   });
 });
 
